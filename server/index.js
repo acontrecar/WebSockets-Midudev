ac21f8e971758842989db8feba4f4860193d4775
acontrecar <acontrecar@gmail.com>
2023-10-13 20:39:05 +0200

    Changes ubuntu

--- server/index.js
+++ server/index.js
@@ -4,13 +4,30 @@ import logger from "morgan";
 import { Server } from "socket.io";
 import { createServer } from "node:http";
 
-const port = process.env.PORT ?? 3000;
+import dotenv from 'dotenv'
+import { createClient } from "@libsql/client/.";
+
+dotenv.config()
+
+const port =  3000;
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   connectionStateRecovery: {},
 });
+
+const db = createClient({
+  url: 'libsql://crack-arclight-acontrecar.turso.io',
+  authToken: process.env.DB_TOKEN
+})
+
+await db.execute(`
+  CREATE TABLE IF NOT EXITS messages (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    content TEXT
+  )
+`)
 
 io.on("connection", (socket) => {
   console.log("a user connected");
@@ -19,8 +36,27 @@ io.on("connection", (socket) => {
     console.log("user disconnected");
   });
 
-  socket.on("chat message", (msg) => {
-    io.emit("chat message", msg);
+  socket.on("chat message", async (msg) => {
+
+    let result
+
+    try {
+      result = await db.execute({
+        sql: `INSERT INTO messages (content) VALUES (:message)`,
+        args: {message : msg}
+      })
+
+      console.log('insertado');
+      
+    } catch (error) {
+      console.log(e);
+      return
+    }
+
+    console.log(result);
+
+    io.emit("chat message", msg , result.lastInsertRowid.toString());
+
   });
 });
 
